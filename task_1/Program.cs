c9f19e1820e45aaeb0840f6f0871ff99b59f8802
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-07 11:30:58 +0500

    done

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+int i = 0;
+
+
+while (int.TryParse(number, out i) == false){
+    Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+while (number.Length != 3){
+    Console.WriteLine("Ошибка. Необходимо трехзначное число, попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+
+int res = (i / 10) % 10;
+
+Console.WriteLine("Вторая цифра числа " + res);
+
+
+
+
+
+
