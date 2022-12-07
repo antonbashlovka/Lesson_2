9df11d07ff35f46ee228e46857aaec38d27d93ef
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-07 12:50:07 +0500

    done task 3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 3: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int intNumber = 0;
+
+while (int.TryParse(number, out intNumber) == false){
+    Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+
+if (intNumber % 7 == 0 && intNumber % 23 == 0){
+    Console.WriteLine("да");
+}else{
+    Console.WriteLine("нет");
+}
