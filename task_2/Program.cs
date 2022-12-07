73c177066c18a50d0b320fe3bab7617478d381c0
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-07 12:41:59 +0500

    done task 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int intNumber = 0;
+
+
+while (int.TryParse(number, out intNumber) == false){
+    Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+while (number.Length < 3){
+    Console.WriteLine("Третьей цифры нет, введите другое число");
+    number = Console.ReadLine();   
+}
+
+int length = number.Length;
+
+double res = Math.Floor(intNumber / Math.Pow(10, length-3)) % 10;
+
+
+Console.WriteLine("Третья цифра числа " + res);
